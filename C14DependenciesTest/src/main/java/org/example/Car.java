5e77b6d40608b931c7f273b7ec98d9868edeece2
migstreva <miguelstreva@outlook.com>
2025-08-19 19:50:07 -0300

    first commit

---
+++ C14DependenciesTest/src/main/java/org/example/Car.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Car {
+    private String wheelType;
+    private String color;
+    private String engine;
+    private String licensePlate;
+}
