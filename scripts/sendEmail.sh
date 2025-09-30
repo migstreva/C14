902bd93fe98f8a5c99a9a03e458a155adee28be7
migstreva <128249230+migstreva@users.noreply.github.com>
2025-09-30 17:54:04 -0300

    Secret variable into ShellScript

--- scripts/sendEmail.sh
+++ scripts/sendEmail.sh
@@ -1,3 +1,3 @@
 cd C14DependenciesTest
 sudo apt-get install mailutils
-echo "Sending email after pipeline completion" | mail -s "Pipeline" miguel.vianna@gec.inatel.br
+echo "Sending email after pipeline completion" | mail -s "Pipeline" "$DEST_EMAIL"
